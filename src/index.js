e5f50b14074c0d176189bde7e92d86d663edc52a
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-10-30 17:51:30 +0400

    feat: change the extension of the App to the tsx, remove unncessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
