68df29a5e19a5e971300d99126adade03c78d313
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-02 11:13:06 +0400

    fix: provide the LoggedIn context to the app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import LoggedInContext from '../../contexts/LoggedInContext';
 
 import Main from '../../pages/Main';
 
 import './App.css';
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(LoggedInContext);
+  const loggedInValue = useMemo(() => ({ isLoggedIn, setIsLoggedIn }), [isLoggedIn, setIsLoggedIn]);
+
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <LoggedInContext.Provider value={loggedInValue}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </LoggedInContext.Provider>
     </div>
   );
 }
