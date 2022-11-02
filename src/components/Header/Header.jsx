f3779d6ac28bdda6d02291e21333368992c2c599
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-02 19:22:21 +0400

    style: add styles for the Navbar component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,22 @@ import LoggedInContext from '../../contexts/LoggedInContext';
 
 import Navbar from '../Navbar/Navbar';
 import Signbar from '../Signbar/Signbar';
+import CustomButton from '../CustomButton/CustomButton';
+
+import headerLogoUrl from '../../images/header-logo.svg';
 
 import './Header.css';
 
 function Header() {
   const { isLoggedIn } = useContext(LoggedInContext);
 
-  return <header className="header">{isLoggedIn ? <Navbar /> : <Signbar />}</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={headerLogoUrl} alt="Логотип сайта" />
+      <CustomButton className="header__menu-button" />
+      {isLoggedIn ? <Navbar /> : <Signbar />}
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import CustomLink from '../CustomLink/CustomLink';
+import ProfileLink from '../ProfileLink/ProfileLink';
 
 import './Navbar.css';
 
@@ -13,24 +15,30 @@ function Navbar() {
           </CustomLink>
         </li>
         <li className="navbar__item">
-          <CustomLink path="/" className="navbar__link" activeClassName="navbar__link_active">
+          <CustomLink
+            path="/simulator"
+            className="navbar__link"
+            activeClassName="navbar__link_active"
+          >
             Тренажёр
           </CustomLink>
         </li>
         <li className="navbar__item">
-          <CustomLink path="/" className="navbar__link" activeClassName="navbar__link_active">
+          <CustomLink path="/memos" className="navbar__link" activeClassName="navbar__link_active">
             Памятки
           </CustomLink>
         </li>
         <li className="navbar__item">
-          <CustomLink path="/" className="navbar__link" activeClassName="navbar__link_active">
+          <CustomLink
+            path="/catalog"
+            className="navbar__link"
+            activeClassName="navbar__link_active"
+          >
             Справочник фракций
           </CustomLink>
         </li>
         <li className="navbar__item">
-          <CustomLink path="/" className="navbar__link" activeClassName="navbar__link_active">
-            Профиль
-          </CustomLink>
+          <ProfileLink path="/profile" commonClassName="navbar__link" text="Профиль" />
         </li>
       </ul>
     </nav>
