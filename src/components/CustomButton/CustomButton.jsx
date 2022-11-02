203d6a09df4c214f65c7293bcb84d803d6f611d7
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-02 09:42:30 +0400

    feat: add the CustomButton component

---
+++ src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './CustomButton.css';
+
+function CustomButton({
+  className, isSubmitButton, onClick, ariaLabel, disabled, children,
+}) {
+  return (
+    <button
+      className={`custom-button ${className}`}
+      type={isSubmitButton ? 'submit' : 'button'}
+      onClick={onClick}
+      aria-label={ariaLabel}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+CustomButton.propTypes = {
+  className: PropTypes.string.isRequired,
+  isSubmitButton: PropTypes.bool,
+  onClick: PropTypes.func,
+  ariaLabel: PropTypes.string,
+  disabled: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+CustomButton.defaultProps = {
+  isSubmitButton: false,
+  onClick: () => {},
+  ariaLabel: '',
+  disabled: false,
+  children: '',
+};
+
+export default CustomButton;
