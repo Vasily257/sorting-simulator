4bfd122d3d308b90857de9fc6ecf9f068bbb9cc8
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-02 19:19:57 +0400

    chore: minor fix

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 
 import './Content.css';
 
-export default function Content({ children }) {
+function Content({ children }) {
   return <main className="content">{children}</main>;
 }
 
 Content.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export default Content;
