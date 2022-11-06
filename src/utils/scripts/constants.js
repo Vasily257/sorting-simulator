e6f70d5f1fc8b629b9e6552c2d0270c3d3ec935f
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-06 12:41:33 +0400

    feat: add hooks to define the screen view

---
+++ src/utils/scripts/constants.js
@@ -0,0 +1,9 @@
+const MEDIA_QUERIES = {
+  IS_MOBILE: '(min-width: 300px)',
+  IS_TABLET: '(min-width: 600px)',
+  IS_DESKTOP: '(min-width: 900px)',
+};
+
+const SOME_CONSTANT = 'Эта постоянная будет удалена';
+
+export { MEDIA_QUERIES, SOME_CONSTANT };
