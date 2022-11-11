a28691e2bf5384451a18ff671f59aa27bf5dce1e
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-11 11:48:20 +0400

    feat: add the Promo component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
+import Promo from '../components/Promo/Promo';
 
 function Main() {
   return (
     <>
       <Header />
-      <Content />
+      <Content>
+        <Promo />
+      </Content>
     </>
   );
 }
