23402ebddfb18a313bdfc3f8c661f38c35a26c13
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-14 11:31:24 +0400

    chore: change promo pictures and add title hiding in mobile view

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
+import useScreenView from '../../hooks/useScreenView';
+
 import Section from '../Section/Section';
 import CustomButton from '../CustomButton/CustomButton';
 
-import promoPictureWebp from '../../images/promo/promo-image.webp';
-import promoPictureJpg from '../../images/promo/promo-image.jpg';
+import promoPictureWebp from '../../images/promo/promo-picture.webp';
+import promoPicturePng from '../../images/promo/promo-picture.png';
 
 import './Promo.css';
 
 function Promo() {
+  const { isTablet } = useScreenView();
   return (
     <Section classNameFromChild="promo">
-      <h1 className="promo__title">Вторсырьё на&nbsp;благотворительность</h1>
+      <h1 className={`promo__title ${!isTablet ? 'visually-hidden' : ''}`}>
+        Вторсырьё на&nbsp;благотворительность
+      </h1>
       <picture className="promo__picture-wrapper">
         <source srcSet={promoPictureWebp} />
         <img
           className="promo__picture"
-          src={promoPictureJpg}
+          src={promoPicturePng}
           alt="Логотип движения Вторсырьё на благотворительность"
         />
       </picture>
