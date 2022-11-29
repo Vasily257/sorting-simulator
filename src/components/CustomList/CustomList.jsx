a5ba05bb22c119196d6c0486e74175d9fd17d59b
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-11-29 19:04:10 +0400

    chore: add an aria-label attribute for the Custom List component

--- src/components/CustomList/CustomList.jsx
+++ src/components/CustomList/CustomList.jsx
@@ -3,17 +3,23 @@ import PropTypes from 'prop-types';
 
 import './CustomList.css';
 
-function CustomList({ className, children }) {
-  return <ul className={`custom-list ${className}`}>{children}</ul>;
+function CustomList({ className, ariaLabel, children }) {
+  return (
+    <ul className={`custom-list ${className}`} aria-label={ariaLabel}>
+      {children}
+    </ul>
+  );
 }
 
 CustomList.propTypes = {
   className: PropTypes.string,
+  ariaLabel: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
 
 CustomList.defaultProps = {
   className: '',
+  ariaLabel: '',
   children: '' || null,
 };
 
