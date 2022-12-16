bd7c5aa0ddf71f14ebbbec20e918a1860990479a
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-12-16 11:37:46 +0400

    chore(App): use CSS Module

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import LoggedInContext from '../../contexts/LoggedInContext';
-
 import Main from '../../pages/Main';
-
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const loggedInValue = useMemo(() => ({ isLoggedIn, setIsLoggedIn }), [isLoggedIn, setIsLoggedIn]);
 
   return (
-    <div className="app">
+    <div className={styles.base}>
       <LoggedInContext.Provider value={loggedInValue}>
         <Routes>
           <Route path="/" element={<Main />} />
