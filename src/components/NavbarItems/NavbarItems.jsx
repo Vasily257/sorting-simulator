35b468d91c6b6f8f0990596628fe456b3cf64e17
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-12-16 10:28:26 +0400

    fix(NavbarItems): fix a name of the active class

--- src/components/NavbarItems/NavbarItems.jsx
+++ src/components/NavbarItems/NavbarItems.jsx
@@ -4,12 +4,12 @@ import CustomLink from '../CustomLink/CustomLink';
 import TEXT_CONTENT from '../../utils/scripts/text-content';
 
 function NavbarItems({ styles }) {
-  const { item, link, activeLink } = styles;
+  const { item, link, linkActive } = styles;
   const { navbarItems } = TEXT_CONTENT.HEADER_NAVIGATION;
 
   return navbarItems.map(({ id, text, path }) => (
     <li key={id} className={item}>
-      <CustomLink path={path} className={link} activeClassName={activeLink}>
+      <CustomLink path={path} className={link} activeClassName={linkActive}>
         {text}
       </CustomLink>
     </li>
